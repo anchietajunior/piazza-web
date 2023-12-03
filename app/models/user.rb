929f42e328c5f327bc966adc8cc9e3912dff9840
José Anchieta <santosjr87@gmail.com>
2023-12-03 14:25:21 -0300

    Add password confirmation and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 8 }
+  validates :password, confirmation: true
 
   private
 
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -17,7 +17,7 @@ class UserControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to root_path
     follow_redirect!
-    assert_select ".notification.is-success", text: I18n.t("users.create.welcome", name: "John")
+    assert_select ".notification.is-success", text: I18n.t("users.create.welcome", name: "Anchieta")
   end
 
   test "renders errors if input data is invalid" do
@@ -36,5 +36,24 @@ class UserControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :unprocessable_entity
     assert_select "p.is-danger", text: I18n.t("activerecord.errors.models.user.attributes.password.too_short")
+  end
+
+  test "renders password confirmation errors if input data is invalid" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_no_difference ["User.count", "Organization.count"] do
+      post sign_up_path, params: {
+        user: {
+          name: "Anchieta",
+          email: "anchieta@redirectto.com",
+          password: "12345678",
+          password_confirmation: "1234567"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "p.is-danger", text: I18n.t("errors.messages.confirmation")
   end
 end
